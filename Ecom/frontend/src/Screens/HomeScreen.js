483ae04227442af1d9c76ce2f9d56f265354aa76
gundrm <rachelm.gundy@gmail.com>
2020-04-09 15:22:48 -0700

    add reducer

--- Ecom/frontend/src/Screens/HomeScreen.js
+++ Ecom/frontend/src/Screens/HomeScreen.js
@@ -1,47 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
 import axios from 'axios';
-
-import {Link} from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 function HomeScreen(props) {
+  const productList = useSelector(state => state.productList);
+  const { products, loading, error } = productList;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(listProducts());
 
-    // define hooks
-    const [products, setProduct] = useState([]);
+    return () => {
+      //
+    };
+  }, [])
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const {data} = await axios.get("/api/products");
-            setProduct(data);
+  return loading ? <div>Loading...</div> :
+    error ? <div>{error}</div> :
+      <ul className="products">
+        {
+          products.map(product =>
+            <li key={product._id}>
+              <div className="product">
+                <Link to={'/product/' + product._id}>
+                  <img className="product-image" src={product.image} alt="product" />
+
+                </Link>
+                <div className="product-name">
+                  <Link to={'/product/' + product._id}>{product.name}</Link>
+                </div>
+                <div className="product-brand">{product.brand}</div>
+                <div className="product-price">${product.price}</div>
+              </div>
+            </li>)
         }
-        fetchData();
-        return () => {
-            //
-        };
-    }, [])
 
-    return <ul className="products">
-            {/* map over products - dynamically */}
-            { products.map(product => 
-                    <li key={product._id}>
-                        <div className="product">
-                            <Link to={'/product/' + product._id}>
-                                <img className="product-image" src={product.image} alt="product"/>
-                            </Link>
 
-                            <div className="product-name">
-                                <Link to={'/product/' + product._id}>
-                                    {product.name}    
-                                </Link>
-                            </div>
 
-                            <div className="product-brand">{product.brand}</div>
-
-                            <div className="product-price">{product.price}</div>
-                            
-                        </div>
-                    </li>
-            )}        
-        </ul>
+      </ul>
 }
-
 export default HomeScreen;
