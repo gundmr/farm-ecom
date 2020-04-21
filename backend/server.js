f0f47628352573c4df6a42c6c917dd535a994e87
gundrm <rachelm.gundy@gmail.com>
2020-04-21 10:57:57 -0700

    make dynamic port for heroku

--- backend/server.js
+++ backend/server.js
@@ -39,5 +39,8 @@ app.get("/api/config/paypal", (req, res) => {
 //   res.send(data.products);
 // });
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+
+//app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
