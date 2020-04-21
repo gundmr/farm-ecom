1ff2b3d05336be9f77d954dc03aeea9f59623187
gundrm <rachelm.gundy@gmail.com>
2020-04-21 11:24:08 -0700

    update server page

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import config from './config';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
@@ -7,7 +6,6 @@ import userRoute from './routes/userRoute';
 import productRoute from './routes/productRoute';
 import orderRoute from './routes/orderRoute';
 
-dotenv.config();
 
 const mongodbUrl = config.MONGODB_URL;
 mongoose.connect(mongodbUrl, {
