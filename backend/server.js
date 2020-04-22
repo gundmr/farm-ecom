d0ee580ac6dce46a6bd4e20ececca2b91cec16d3
gundrm <rachelm.gundy@gmail.com>
2020-04-22 08:56:11 -0700

    add sendFile in server

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,13 @@ app.get("/api/config/paypal", (req, res) => {
 //   res.send(data.products);
 // });
 
+//app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '/frontend/index.html')) ) ;
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
