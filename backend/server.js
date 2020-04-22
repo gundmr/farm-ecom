da9805543fb57b549823d057ab88918666c3e66a
gundrm <rachelm.gundy@gmail.com>
2020-04-22 09:04:38 -0700

    server update II

--- backend/server.js
+++ backend/server.js
@@ -37,11 +37,11 @@ app.get("/api/config/paypal", (req, res) => {
 //   res.send(data.products);
 // });
 
-//app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '/frontend/index.html')) ) ;
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '/frontend/index.html')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
 
 
 const PORT = process.env.PORT || 5000;
