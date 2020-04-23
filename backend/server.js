fd4e4affb49bff93b4b8ffdc653be0f956d4395d
gundrm <rachelm.gundy@gmail.com>
2020-04-23 11:57:35 -0700

    update dist and npm packages

--- backend/server.js
+++ backend/server.js
@@ -24,33 +24,13 @@ app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || config.PAYPAL_CLIENT_ID);
 })
 
-// app.get("/api/products/:id", (req, res) => {
-//   const productId = req.params.id;
-//   const product = data.products.find(x => x._id === productId);
-//   if (product)
-//     res.send(product);
-//   else
-//     res.status(404).send({ msg: "Product Not Found." })
-// });
+// try next
+app.use(express.static(__dirname + '/frontend'))
 
-// app.get("/api/products", (req, res) => {
-//   res.send(data.products);
-// });
 
-//app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '/frontend/index.html')));
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// });
-
-const path = require('path');
-const serveStatic = require('serve-static');
-
-app.use(serveStatic(path.join(__dirname, 'dist')));
 
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
 
-//app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
--- dist/server.js
+++ dist/server.js
@@ -33,28 +33,10 @@ app.use("/api/products", _productRoute["default"]);
 app.use("/api/orders", _orderRoute["default"]);
 app.get("/api/config/paypal", function (req, res) {
   res.send(process.env.PAYPAL_CLIENT_ID || _config["default"].PAYPAL_CLIENT_ID);
-}); // app.get("/api/products/:id", (req, res) => {
-//   const productId = req.params.id;
-//   const product = data.products.find(x => x._id === productId);
-//   if (product)
-//     res.send(product);
-//   else
-//     res.status(404).send({ msg: "Product Not Found." })
-// });
-// app.get("/api/products", (req, res) => {
-//   res.send(data.products);
-// });
-//app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '/frontend/index.html')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// });
+}); // try next
 
-var path = require('path');
-
-var serveStatic = require('serve-static');
-
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(_express["default"]["static"](__dirname + '/frontend'));
 var PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
   return console.log("Server started on PORT ".concat(PORT));
-}); //app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
+});
